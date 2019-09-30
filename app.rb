0b77b96d5fed51b352ff5141df757ab115a6eaf5
Brad Phillips <bjrp@protonmail.com>
2019-09-30 15:54:35 +0100

    Updates app.rb to include cat image with border

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "Hello world"
 end
 
 get '/secret' do
   "Batman is Bruce Wayne"
+end
+
+get '/recipe' do
+  "Try toast + jam"
+end
+
+get '/holidaytips' do
+  "Why not visit Thailand?"
+end
+
+require 'sinatra'
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
